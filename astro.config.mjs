c6cd01b91097b0e8068efd454a9ca5e5e2d4c021
AhmetOzkaan <ozkanahmet272@gmail.com>
2025-05-05 11:25:04 +0300

    feat: add @astrojs/node dependency to configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,19 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
-import { access } from "fs";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://youmis.org/",
+
   server: {
     port: 80,
     host: true,
     allowedHosts: ["youmis.org"]
   },
+
   integrations: [
     tailwind(),
     icon(),
@@ -24,9 +27,14 @@ export default defineConfig({
       },
     }),
   ],
+
   env: {
     schema: {
       GOOGLE_API_KEY: envField.string({ context: "server", access: "secret"}),
     }
-  }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
